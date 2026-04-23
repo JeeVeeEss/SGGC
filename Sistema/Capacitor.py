7537a73690af7f272f4cde54f5df31a879a31a0b
= <guivicente616@gmail.com>
2026-04-23 00:51:58 -0300

    Modificação no Main.py: Funcionamento parcial.

--- Sistema/Capacitor.py
+++ Sistema/Capacitor.py
@@ -1,6 +1,11 @@
 from ComponenteEletrico import ComponenteEletrico
 class Capacitor(ComponenteEletrico):
     def __init__(self, nome, fabricante, capacitancia):
-        super().__init__(nome, fabricante) # Método para inicializar as variáveis herdadas pelo ComponenteEletrico
+        super().__init__(nome, fabricante, tensao_max=None) # Método para inicializar as variáveis herdadas pelo ComponenteEletrico
         self.capacitancia = capacitancia
-        
+
+    def calcular_impedancia(self):
+        impedancia = 1/(2*(3,1415)*60*self.capacitancia)
+    
+    def __str__(self):
+        return self.nome, self.fabricante, self.capacitancia[0].capacitancia
--- Sistema/main.py
+++ Sistema/main.py
@@ -11,15 +11,18 @@ def main():
     os.system('cls')
     tensao = configurar_circuito()
     circuito = Circuito(tensao=tensao)
-    print("-------------\n-R(resistor)\n-C(capacitor)\n-X(Sair do programa)")
-    
+
     while True:
+        #os.system('cls')
+        print("-------------\n-R(resistor)\n-C(capacitor)\n-X(Sair do programa)")
         operacao = str(input("Digite o componente a ser adicionado ao sistema (nome e fabricante) separados por espaço(" ") : "))
         if operacao == 'X' or operacao == 'x':
             break
         elif operacao == 'c' or operacao == 'C':
             dados  = str(input('Digite o valor desejado( Resistência(ohms)/ Capacitância(faraday):'))
             dados = dados.split(' ')
-            circuito.adicionar_componente(operacao, list(Capacitor(dados[0], dados[1], dados[2])))
-        print(circuito, circuito.resistores, circuito.capacitores)
+            circuito.adicionar_componente(operacao, Capacitor(dados[0], dados[1], dados[2]))
+        elif operacao == 'r' or operacao == 'R':
+            circuito.adicionar_componente(operacao, Resistor(dados[0], dados[1], dados[2]))
+        print(circuito, circuito.resistores, circuito.capacitores) # Apenas para teste, sem função estrutural.
 main()
